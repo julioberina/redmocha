e9d5e938b2b6fbe297213e32c5787d8258e20e6b
Julio Berina <julioberina@gmail.com>
2017-02-02 18:44:34 -0800

    Erased class for further fixing

--- lib/redmocha.rb
+++ lib/redmocha.rb
@@ -1,7 +1,17 @@
 path = File.expand_path(File.dirname(__FILE__))
 $LOAD_PATH.unshift path
-
 require "java"
+
+class String
+  def underscore
+    self.gsub(/::/, '/').
+    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+    gsub(/([a-z\d])([A-Z])/,'\1_\2').
+    tr("-", "_").
+    downcase
+  end
+end
+
 Dir["#{path}/gdxlibs/\*.jar"].each do |jar|
   require jar.sub("#{path}/", "")
 end
@@ -11,34 +21,13 @@ Dir["#{path}/includes/\*.rb"].each do |rb|
 end
 
 def rm_import(klass)
-  
+  klass = klass.to_s.split("::")
+  klass[klass.length-1] = klass.last.underscore.split("_").map { |w| w.upcase }.join("_")
+  java_import klass.join("::")
 end
 
 def rm_include(interface)
-  
-end
-
-class RMScreen
-  include RedMocha::Screen
-  
-  def initialize(game = nil)
-    @game = game
-  end
-end
-
-class RMGame < RedMocha::Game
-  attr_accessor :title, :width, :height
-
-  def initialize(title = "RedMocha Game", width = 800, height = 600)
-    @title = title
-    @width = width
-    @height = height
-  end
-
-  def create
-  end
-
-  def run
-    LwjglApplication.new(self, @title, @width, @height)
-  end
+  interface = interface.to_s.split("::")
+  interface[interface.length-1] = interface.last.underscore.split("_").map { |w| w.upcase.join("_") }
+  include interface.join("::")
 end
