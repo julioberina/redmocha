c02d743d76bec0569a5ad6f7d68b5dfd257f4d6f
Julio Berina <julioberina@gmail.com>
2017-02-04 11:55:12 -0800

    Created RMGame and RMScreen, defaults for setting up multiple screens

--- lib/redmocha.rb
+++ lib/redmocha.rb
@@ -31,3 +31,72 @@ def rm_include(interface)
   interface[interface.length-1] = interface.last.underscore.split("_").map { |w| w.upcase.join("_") }
   include interface.join("::")
 end
+
+class RMScreen
+  include com.badlogic.gdx.Screen
+
+  def initialize(game)
+    @game = game
+  end
+
+  protected
+
+  def render(delta)
+    com.badlogic.gdx.Gdx.gl.glClearColor(0.0, 0.0, 0.0, 1.0)
+    com.badlogic.gdx.Gdx.gl.glClear(com.badlogic.gdx.graphics.GL20::GL_COLOR_BUFFER_BIT)
+
+    @game.batch.begin
+    @game.font.draw(@game.batch, "RedMocha beats white mocha fam!", 20, 20)
+    @game.batch.end
+  end
+
+  def show
+  end
+
+  def hide
+  end
+
+  def dispose
+  end
+
+  def pause
+  end
+
+  def resize(width, height)
+  end
+
+  def resume
+  end
+end
+
+class RMGame < com.badlogic.gdx.Game
+  attr_accessor :batch, :font, :title, :width, :height, :screen
+  
+  def create
+    @batch = com.badlogic.gdx.graphics.g2d.SpriteBatch.new
+    @font = com.badlogic.gdx.graphics.g2d.BitmapFont.new
+    @screen = RMScreen.new(self)
+    self.set_screen(@screen)
+  end
+
+  def render
+    super
+  end
+
+  def dispose
+    @batch.dispose unless @batch.nil?
+    @font.dispose unless @font.nil?
+  end
+
+  def run
+    com.badlogic.gdx.backends.lwjgl.LwjglApplication.new(self, @title, @width, @height)
+  end
+
+  protected
+
+  def initialize(title = "RedMocha Game", width = 800, height = 600)
+    @title = title
+    @width = width
+    @height = height
+  end
+end
